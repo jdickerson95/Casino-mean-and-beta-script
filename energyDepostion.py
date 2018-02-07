dce23aa56a9fe5f871a0d66bb7a1a742abef9fc7
jdickerson95 <32930629+jdickerson95@users.noreply.github.com>
2018-02-07 13:12:39 +0000

    Updated for fitting

--- energyDepostion.py
+++ energyDepostion.py
@@ -4,13 +4,15 @@ import numpy
 from scipy.stats import gumbel_l
 
 def loop(inputFile='.dat'):
-  #  vals = ["1", "2.5", "5", "7.5", "10", "11.25", "12.5","13.75","15","17.5","20","22.5","25","27.5","30","32.5","35","37.5","40","42.5","45","47.5","50","55","60","65","70","75","80","85","90","95","100"]
-    vals = ["10"]
-    overall = []
+   # vals = ["1", "2.5", "5", "7.5", "10", "11.25", "12.5","13.75","15","17.5","20","22.5","25","27.5","30","32.5","35","37.5","40","42.5","45","47.5","50","55","60","65","70","75","80","85","90","95","100"]
+   # vals = ["10"]
+   # vals = ["1", "2","2.5", "3","4","5"]
+    vals = ["4","5", "7.5", "10", "11.25", "12.5","13.75","15","17.5","20","22.5","25","27.5","30","32.5","35","37.5","40","42.5","45","47.5","50","55","60","65","70","75","80","85","90","95","100"]
+    overall = [["Beam energy", "6", "5", "4", "3", "2", "1", "0"]]
     for v in vals:
         totalDistance = 0.
         totalEnergy = 0.
-        energyBins = int(round(3 * float(v)))
+        energyBins = int(round(5 * float(v)))
        # energyBins = 10
         counter = 0
         start_x = 0
@@ -62,11 +64,13 @@ def loop(inputFile='.dat'):
                                 index = 0
                                 limit = 1/float(energyBins)
                                 for x in range(len(allDistances)):
-                                    if allDistances[x] / totalDistance > limit:
-                                        index += 1
-                                        limit += 1/float(energyBins)
-                                        if index == energyBins:
-                                            index -= 1
+                                    if (allDistances[x] / totalDistance) > limit: #this not quite right need to start at correct index as atm just puts in second whatever
+                                        while (allDistances[x] / totalDistance) > limit:
+                                            index += 1
+                                            limit += 1/float(energyBins)
+                                            if index == energyBins:
+                                                index -= 1
+                                                break
                                     rangeEnergy[index] += allEnergies[x]
 
                                 overallRangeEnergy.append(rangeEnergy)
@@ -104,9 +108,16 @@ def loop(inputFile='.dat'):
             averageRangeEnergy.append(totAverageEnergy/length)
         proportionRangeEnergy = []
         totalEnergy = sum(averageRangeEnergy) #should be v-0.05
+        '''
         for i in averageRangeEnergy:
             proportionRangeEnergy.append(i/totalEnergy)
 
+        firstProportionRangeEnergy = []
+        for i in proportionRangeEnergy:
+         #   usedProportionRangeEnergy.append(i) #if all
+            if i < 0.2:  #if just start
+                firstProportionRangeEnergy.append(i)
+         '''
        # std_numpy = numpy.std(allDistances)
        # beta = (std_numpy * math.sqrt(6))/math.pi
        # mean_numpy = numpy.mean(allDistances)
@@ -114,15 +125,32 @@ def loop(inputFile='.dat'):
      #   meanDistance = totalDistance / counter
       #  allDistances.sort()
       #  loc, scale = gumbel_l.fit(allDistances)
+
         for i in range(energyBins):
             proportionAlongTrack = (i*(1/float(energyBins)) + (i+1)*(1/float(energyBins)))/2
-            overall.append([v, proportionAlongTrack, proportionRangeEnergy[i]])
+            overall.append([v, proportionAlongTrack, averageRangeEnergy[i]])
+
+
+        proportionAlongTrack = []
+        firstAverageEnergy = []
+        loopCounter = -1
+       # for i in range(energyBins): #all
+        for i in range(int(energyBins * (2/float(v)))): #first few points
+            loopCounter += 1
+            proportionAlongTrack.append((i*(1/float(energyBins)) + (i+1)*(1/float(energyBins)))/2)
+            firstAverageEnergy.append(averageRangeEnergy[loopCounter])
+
+
+        x_six, x_five, x_four, x_three, x_two, x_one, x_zero = numpy.polyfit(proportionAlongTrack, firstAverageEnergy, 6)
+        overall.append([v, x_six, x_five, x_four, x_three, x_two, x_one, x_zero])
 
     f = open('./energy-depostion.csv', 'wb')
     # f = open('./low-mode.csv', 'wb')
     writer = csv.writer(f)
     writer.writerows(overall)
     f.close()
+
+
 
 if __name__ == "__main__":
     loop()
